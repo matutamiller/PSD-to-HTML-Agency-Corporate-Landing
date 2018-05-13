205aba899e9b64e10b3dcd4785ab0f219b5b0fcc
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-05-13 22:01:09 +0200

    Added 'contacts' section

--- script.js
+++ script.js
@@ -139,3 +139,13 @@
         document.querySelector('.sliderTextNav li').className = 'active';
     })
 })();
+
+//Google map
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
